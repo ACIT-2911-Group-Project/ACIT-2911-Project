9b918ef8a64f3f0273f9e6ee3380425464521f54
RJ2506 <77298403+RJ2506@users.noreply.github.com>
2021-05-15 21:43:45 -0700

    added a style in the insert frame

--- ui/insert_frame.py
+++ ui/insert_frame.py
@@ -2,51 +2,59 @@ from Models.movie_review import Movie
 from business.data_access_helper import DataAccessHelper
 
 from tkinter import *
-from tkinter.ttk import Combobox, Treeview
-
+from tkinter.ttk import Combobox,Style, Treeview
 
 class InsertFrame(Frame):
   
   def __init__(self, parent):
-    
+    #initialize the insert frame
     Frame.__init__(self, parent)
+
+    #background color and style
+    background = "#000C66"
+    lbl_color = 'white'
+    self.configure(bg=background) 
     
+
     self._parent = parent
-        
+
+    #set the insert window frame    
     self._insertWindow = Toplevel(self._parent)
     self._insertWindow.transient(parent)
-    self._insertWindow.grab_set()    
-    
-    
+    self._insertWindow.grab_set()
+    self._insertWindow.configure(bg=background) 
+
+    #The windows title and the size
     self._insertWindow.title("Insert Movie Review")
     self._insertWindow.geometry('350x300')
     
+    #Define the string variables for the column
     self._movie_name = StringVar()
     self._movie_year = StringVar()
     self._movie_rating = StringVar()
     self._movie_genre = StringVar()
     self._movie_review = StringVar()
-    
-    
-    lbl_name = Label(self._insertWindow, text='Name:', font=('bold', 12), padx=5, pady=5)
+     
+    #create the label for the name, year, rating, and genre with an entry field
+    lbl_name = Label(self._insertWindow, text='Name:', font=('bold', 12), padx=5, pady=5, fg=lbl_color, bg=background)
     lbl_name.grid(row=0, column=0, sticky="W")
 
     entry_name = Entry(self._insertWindow, width=30, textvariable=self._movie_name)
     entry_name.grid(row=0, column=1)
     
-    lbl_year = Label(self._insertWindow, text='Year:', font=('bold', 12), padx=5, pady=5)
+    lbl_year = Label(self._insertWindow, text='Year:', font=('bold', 12), padx=5, pady=5,fg=lbl_color, bg=background)
     lbl_year.grid(row=1, column=0, sticky="W")
 
     entry_year = Entry(self._insertWindow, width=30, textvariable=self._movie_year)
     entry_year.grid(row=1, column=1)
     
-    lbl_rating = Label(self._insertWindow, text='Rating:', font=('bold', 12), padx=5, pady=5)
+    lbl_rating = Label(self._insertWindow, text='Rating:', font=('bold', 12), padx=5, pady=5,fg=lbl_color, bg=background)
     lbl_rating.grid(row=2, column=0, sticky="W")
 
     entry_rating = Entry(self._insertWindow, width=30, textvariable=self._movie_rating)
     entry_rating.grid(row=2, column=1)
     
-    lbl_genre = Label(self._insertWindow, text='Genre:', font=('bold', 12), padx=5, pady=5)
+    lbl_genre = Label(self._insertWindow, text='Genre:', font=('bold', 12), padx=5, pady=5,fg=lbl_color, bg=background)
     lbl_genre.grid(row=3, column=0, sticky="W")
 
     genres=('Horror', 'Action', 'Romance', 'Comedy', 'Science Fiction', 'Drama', 'Thriller', 'Documentary', 'Western', 'Musical', 'Crime', 'Fiction', 'Romantic Comedy', 'Music', 'War', 'Adventure', 'Epic')
@@ -56,16 +64,16 @@ class InsertFrame(Frame):
     cbox_genre['state'] = 'readonly'  # normal    
     #cbox_genre.bind('<<ComboboxSelected>>', genre_selected)  
 
-    lbl_review = Label(self._insertWindow, text='Review:', font=('bold', 12), padx=5, pady=5)
+    lbl_review = Label(self._insertWindow, text='Review:', font=('bold', 12), padx=5, pady=5,fg=lbl_color, bg=background)
     lbl_review.grid(row=6, column=0, sticky="W")
 
     self._entry_review = Text(self._insertWindow, width=40, height=5, padx=5)
-    self._entry_review.grid(row=7, column=0, columnspan=2, sticky="W")
+    self._entry_review.grid(row=7, column=0, columnspan=2, padx=8, sticky="W")
     
-    insert_btn = Button(self._insertWindow, text='Add Review', width=12, command=self.btnClickInsertReview)
+    insert_btn = Button(self._insertWindow, text='Add Review', width=12, command=self.btnClickInsertReview, fg=lbl_color, bg='#2ECC71')
     insert_btn.grid(row=10, column=0, sticky="W", padx=5, pady=15)
 
-    cancel_btn = Button(self._insertWindow, text='Cancel', width=12, command=self.btnClickCancel)
+    cancel_btn = Button(self._insertWindow, text='Cancel', width=12, command=self.btnClickCancel, fg=lbl_color, bg='#E74C3C')
     cancel_btn.grid(row=10, column=1, sticky="E", padx=5, pady=15)       
 
     #self.pack()
@@ -96,10 +104,6 @@ class InsertFrame(Frame):
       
   def btnClickCancel(self):
       self._insertWindow.destroy()
-    
-            
 
-    
-     
-    
+ 
 
